c8432d75d60c95d35ac938b52acaa7db04975d8f
el-kg <131970333+el-kg@users.noreply.github.com>
2024-04-07 10:52:35 +0700

    Возникли сложности с подключением JUnit и началом тестирования

---
+++ src/tests/com/gridnine/testing/DepartureBeforeNowFilterTest.java
@@ -0,0 +1,10 @@
+package tests.com.gridnine.testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartureBeforeNowFilterTest {
+
+    @org.junit.jupiter.api.Test
+    void filter() {
+    }
+}
